9bbbcaf71d1c9b335f884a4d2d0047278d9e0a7f
Cássio Cornachi <cassiocornachi@gmail.com>
2026-07-29 17:12:25 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import ScrollToTop from './components/ScrollToTop';
+import ProtectedRoute from '@/components/ProtectedRoute';
 // Add page imports here
 import Home from './pages/Home';
 import Equipe from './pages/Equipe';
@@ -14,6 +15,10 @@ import Ranking from './pages/Ranking';
 import SimularPartida from './pages/SimularPartida';
 import Loja from './pages/Loja';
 import Estadio from './pages/Estadio';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 import Layout from './components/Layout';
 
 const AuthenticatedApp = () => {
@@ -43,14 +48,20 @@ const AuthenticatedApp = () => {
   return (
     <Routes>
       {/* Add your page Route elements here */}
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/equipe" element={<Equipe />} />
-        <Route path="/simular-partida" element={<SimularPartida />} />
-        <Route path="/resultado-partida" element={<ResultadoPartida />} />
-        <Route path="/ranking" element={<Ranking />} />
-        <Route path="/loja" element={<Loja />} />
-        <Route path="/estadio" element={<Estadio />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route element={<ProtectedRoute unauthenticatedElement={<Navigate to="/login" replace />} />}>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/equipe" element={<Equipe />} />
+          <Route path="/simular-partida" element={<SimularPartida />} />
+          <Route path="/resultado-partida" element={<ResultadoPartida />} />
+          <Route path="/ranking" element={<Ranking />} />
+          <Route path="/loja" element={<Loja />} />
+          <Route path="/estadio" element={<Estadio />} />
+        </Route>
       </Route>
       <Route path="*" element={<PageNotFound />} />
     </Routes>
