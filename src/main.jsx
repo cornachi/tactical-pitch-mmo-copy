2c614383fbefdcd5a3ecb576280fb97fd2d0885d
cornachi <cassiocornachi@gmail.com>
2026-08-05 22:01:21 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
+window.BASE44_SERVER_URL = "https://tactical-pitch-mmo-copy-c24c4540.base44.app";
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
