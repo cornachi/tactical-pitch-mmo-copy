e178ffa3b4f84a3ce05b5b6f98d9a2fdbdf124bc
cornachi <cassiocornachi@gmail.com>
2026-08-05 22:04:18 -0300

    Update base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -3,12 +3,12 @@ import { appParams } from '@/lib/app-params';
 
 const { appId, token, functionsVersion, appBaseUrl } = appParams;
 
-//Create a client with authentication required
+// Create a client with authentication required
 export const base44 = createClient({
   appId,
   token,
   functionsVersion,
-  serverUrl: '',
+  serverUrl: 'https://tactical-pitch-mmo-copy-c24c4540.base44.app',
   requiresAuth: false,
   appBaseUrl
 });
